62cb5653883f93e9450126bb486e2b2492693516
Elber Lucas Alves Barboza <142125443+4Elber@users.noreply.github.com>
2025-02-01 00:20:34 -0400

    Update envia.php

--- envia.php
+++ envia.php
@@ -1,20 +1,22 @@
 <?php
 
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $tell = addcslashes($_POST['telefone']);
+$nome = htmlspecialchars($_POST['nome']);
+$email = htmlspecialchars($_POST['email']);
+$tell = htmlspecialchars($_POST['telefone']);
 
-    $para = "elberlucasalvesbarboza@gmail.com";
-    $assunto = "Coleta de dados - Portfolio";
+$para = "elberlucasalvesbarboza@gmail.com";
+$assunto = "Coleta de dados - Portfolio";
 
-    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Telefone: ".$tell;
+$corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Telefone: ".$tell;
 
-    $cabeca = "From: teste@inteliogia.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+$cabecalho = "From: teste@inteliogia.com"."\r\n".
+             "Reply-To: ".$email."\r\n".
+             "X-Mailer: PHP/".phpversion();
 
-    if(mail($para,$assunto,$corpo,$cabeca)){
-        echo("Email enviado com sucesso!");
-    }else{
-        echo("Houve um erro ao enviar o email!");
-    }
+if(mail($para, $assunto, $corpo, $cabecalho)){
+    echo "Email enviado com sucesso!";
+} else {
+    echo "Houve um erro ao enviar o email: " . error_get_last()['message'];
+}
 
 ?>
